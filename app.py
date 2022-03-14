1fd0817366b67731263b59f84c322347ae818936
DrSquishGod <18408@wc.school.nz>
2022-03-15 11:13:28 +1300

    Inserted images, changed app.py, change smile.css, changed menu.html and was about to delete database and redo it which is in video 4

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
 from flask import Flask, render_template, request, session
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = "smile.db"
 
 app = Flask(__name__)
 
 
+def create_connection(db_file):
+    """create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        # intialise_tables(connection)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
+
+
 @app.route('/')
 def render_homepage():
-    #return '<h1>Hello, World!</h1>'
     return render_template("home.html")
+
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template("menu.html")
+    # connect to the database
+    con = create_connection(DB_NAME)
+
+    # SELECT the things you want from your table(s)
+    query = "SELECT name, description, volume, price, image FROM product"
+
+    cur = con.cursor()  # You need this line next
+    cur.execute(query)  # This line actually executes the query
+    product_list = cur.fetchall()  # Puts the results into a list usable in python
+    con.close()
+
+    return render_template("menu.html", products=product_list)
+
 
 @app.route('/contact')
 def render_contact():
     return render_template("contact.html")
+
 
 app.run(host="0.0.0.0")
