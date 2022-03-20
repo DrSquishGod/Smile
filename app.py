d7482b0a3bd0cb79813a85b97681a3591167e36c
DrSquishGod <18408@wc.school.nz>
2022-03-21 12:25:51 +1300

    Committing changes for all the html files

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session
 import sqlite3
 from sqlite3 import Error
 
+#DB_NAME = "C:/Users/18408/PycharmProjects/smilev2/smile.db"
 DB_NAME = "smile.db"
 
 app = Flask(__name__)
@@ -21,28 +22,52 @@ def create_connection(db_file):
 
 @app.route('/')
 def render_homepage():
-    return render_template("home.html")
+    return render_template('home.html')
 
 
 @app.route('/menu')
 def render_menu_page():
+
     # connect to the database
     con = create_connection(DB_NAME)
 
     # SELECT the things you want from your table(s)
     query = "SELECT name, description, volume, price, image FROM product"
 
-    cur = con.cursor()  # You need this line next
-    cur.execute(query)  # This line actually executes the query
-    product_list = cur.fetchall()  # Puts the results into a list usable in python
+    cur = con.cursor() # You need this line next
+    cur.execute(query) # This line actually executes the query
+    product_list = cur.fetchall() # Puts the results into a list usable in python
     con.close()
 
-    return render_template("menu.html", products=product_list)
+    return render_template("menu.html", products = product_list)
 
 
 @app.route('/contact')
 def render_contact():
     return render_template("contact.html")
 
+
+@app.route('/login')
+def render_login_page():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def render_signup_page():
+    print(request.form)
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+    return render_template('signup.html')
+
+    con = create_connection(DB_NAME)
+
+    query = "INSERT INTO customer(id, fname, lname, email, password) VALUES(NULL, ?, ?, ?, ?)"
+
+    cur = con.cursor()  # You need this line next
+    cur.execute(query, (fname, lname, email, password))  # This line actually executes the query
+    con.commit()
+    con.close()
 
 app.run(host="0.0.0.0")
